1655d515a9c4fecb1e6b29f06b7298f0dcdce388
Sebastian Sanchez <sebastiansanchezg23@gmail.com>
2020-02-21 19:38:36 -0600

    Puse "hola cola gg"

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -15,7 +15,7 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola cola");
+        System.out.println("hola cola gg");
     }
 
 }
