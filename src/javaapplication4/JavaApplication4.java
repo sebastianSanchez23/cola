b950cb182b0cf24c6e2adbd6e220946e1b843d90
Sebastian Sanchez <sebastiansanchezg23@gmail.com>
2020-02-21 19:40:34 -0600

    Cambió a "hola crayola"

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -15,7 +15,7 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola cola gg");
+        System.out.println("hola crayola");
     }
 
 }
